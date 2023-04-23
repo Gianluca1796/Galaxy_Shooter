5d28ee94210cca893f1ee6f7de93cf1a3c36d647
Gianluca Fava <101229939+Gianluca1796@users.noreply.github.com>
2023-04-23 10:57:02 -0300

    Add script for player movement

--- Assets/2D Galaxy Assets/Scripts/Player.cs
+++ Assets/2D Galaxy Assets/Scripts/Player.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float speed = 5.0f;
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        playerMovement();
+    }
+
+
+
+
+    private void playerMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float vericalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * vericalInput * Time.deltaTime);
+
+
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y < -4.2f)
+        {
+
+            transform.position = new Vector3(transform.position.x, -4.2f, 0);
+        }
+
+        if (transform.position.x > 9.5)
+        {
+            transform.position = new Vector3(-9.5f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -9.5f)
+        {
+            transform.position = new Vector3(9.5f, transform.position.y, 0);
+        }
     }
 }
